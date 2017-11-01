b755955ceeeea5a86a3ea7ce3a030ed5e85706af
Micaela Ramirez <ramirezmibe@gmail.com>
2017-10-31 22:28:00 -0300

    Add inicial usuarios con misma mecánica que Libros

---
+++ gestor-de-libros/src/usuarios/Usuario.java
@@ -0,0 +1,32 @@
+package usuarios;
+
+import entidades.Libro;
+
+class Usuario {
+	private String usuario;
+	private String contrasenia;
+	
+	@Override
+	public boolean equals(Object obj) {
+		return this == obj || (obj instanceof Usuario && 
+				(usuario.equals(((Usuario) obj).usuario)) && (contrasenia.equals(((Usuario) obj).contrasenia)));
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getContrasenia() {
+		return contrasenia;
+	}
+
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+
+	
+}
---
+++ gestor-de-libros/src/usuarios/Usuarios.java
@@ -0,0 +1,56 @@
+package usuarios;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintStream;
+import java.util.Scanner;
+import java.util.Vector;
+
+
+public class Usuarios extends Vector<Usuario> {
+	private String RutaArchivo;
+	
+	
+	public Usuarios(String ruta) {
+		RutaArchivo = ruta;
+		cargarUsuariosDesdeArchivo();
+	}
+
+	/**
+	 * Hace la carga inicial del vector desde un archivo.
+	 */
+	private void cargarUsuariosDesdeArchivo() {
+		Usuario us;
+		String[] campos;
+		try {
+			Scanner entrada = new Scanner(new FileReader(RutaArchivo));
+			while (entrada.hasNextLine()) {
+				campos = entrada.nextLine().split("\t");
+				us = new Usuario();
+				us.setUsuario(campos[0]);
+				us.setContrasenia(campos[1]);
+				this.add(us);
+			}
+			entrada.close();
+		} catch (FileNotFoundException e) {
+		}
+	}
+	
+	/**
+	 * Vuelca la información del vector a un archivo.
+	 */
+	public void guardarLibrosEnArchivo() {
+		try {
+			int i;
+			PrintStream salida = new PrintStream(RutaArchivo);
+			Usuario us;
+			for (i = 0; i < size(); i++) {
+				us = get(i);
+				salida.print(us.getUsuario() + "\t");
+				salida.print(us.getContrasenia() + "\t");
+			}
+			salida.close();
+		} catch (FileNotFoundException e) {
+		}
+	}
+}
